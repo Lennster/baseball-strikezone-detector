90387710f33066d532cf0fa476d92f6832abe02b
Lennster <mldeepcove@gmail.com>
2016-05-23 16:33:45 -0700

    Two LDR sesnors now working in all cases
    Fixed the issue with the first sensor triggering a ball or strike. We
    added logic to wait for a second trigger. If the second trigger is the
    other sensor then strike if its the first sensor going back to light
    then ball. Also added logic so that if a strike is detected, we wait for
    both the sensors to go back to light before waiting for a new trigger.

--- two_LDR_strikezone.py
+++ two_LDR_strikezone.py
@@ -4,37 +4,57 @@ from gpiozero import LightSensor
 from time import sleep
 from signal import pause
 
-horizSensor = LightSensor(27,queue_len = 2)
-vertSensor = LightSensor(17,queue_len = 2)
+horizSensor = LightSensor(27,queue_len = 1)
+vertSensor = LightSensor(17,queue_len = 1)
 
 class StrikeZone():
     def __init__(self):
-        self.horizDetected = False
+        self.horizDetected = False 
         self.vertDetected = False
+        self.waitForOtherSensor = True
+        self.clearingStrike = 0
 
     def horizDark(self, sensor):
         print("Detected dark on horiz sensor (Pin:%u)"%sensor.pin.number)
         self.horizDetected = True
         self.checkStrike()
 
-    def horizLight(self,sensor):
-        print("Detected light on horiz sensor (Pin:%u)"%sensor.pin.number)
-        self.horizDetected = False
-
     def vertDark(self,sensor):
         print("Detected dark on vert sensor (Pin:%u)"%sensor.pin.number)
         self.vertDetected = True
         self.checkStrike()
 
+    def horizLight(self,sensor):
+        print("Detected light on horiz sensor (Pin:%u)"%sensor.pin.number)
+        self.horizDetected = False
+        if self.clearingStrike == 0:
+            self.checkStrike()
+        else:
+            self.clearingStrike = self.clearingStrike - 1
+
     def vertLight(self,sensor):
         print("Detected light on vert sensor (Pin:%u)"%sensor.pin.number)
         self.vertDetected = False
+        if self.clearingStrike == 0:
+            self.checkStrike()
+        else:
+            self.clearingStrike = self.clearingStrike - 1
 
     def checkStrike(self):
-        if self.horizDetected and self.vertDetected:
-            print("Strike")
+        if not self.waitForOtherSensor:
+            if self.horizDetected and self.vertDetected:
+                print("Strike")
+                # Once we get a strike we need to wait for both sensors to go
+                # back to light before checking for strikes or balls again
+                self.clearingStrike = 2
+            else:
+                print("Ball")
+            # Once we have determined it is a ball or strike need to go back to
+            # wait for two triggers again
+            self.waitForOtherSensor = True
         else:
-            print("Ball")
+            # Now we have detected one trigger lets just wait for one more
+            self.waitForOtherSensor = False
 
 
 
